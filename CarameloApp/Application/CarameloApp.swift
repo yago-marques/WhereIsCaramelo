475c987093ca363a9e2579dd3660935bc2ecb623
CapistranoIFCE <101426741+CapistranoIFCE@users.noreply.github.com>
2023-04-11 09:20:20 -0300

    Onboarding corrigida

--- CarameloApp/Application/CarameloApp.swift
+++ CarameloApp/Application/CarameloApp.swift
@@ -13,7 +13,7 @@ struct CarameloApp: App {
 
     var body: some Scene {
         WindowGroup {
-            CitiesListComposer.make()
+            OnBoardingView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
